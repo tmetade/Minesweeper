8a7633cf8f51fe93f3f3a74b2eb0478707c91082
Tyler Metade <tmetade@yahoo.ca>
2018-12-11 15:39:59 -0500

    changed theme of UI

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,19 +1,14 @@
-//TODO
-//comment this bad boy
-//make it look nice
-//group related functions
-
 var timeValue = 0;
 var columns = 9;
 var rows = 9;
 var mineCount = 10;
-var flagCountValue = mineCount;
-var tiles = [];
-var coordinates = null;
-var mines = [];
-var isFirstTurn = true;
-var remainingTiles = columns * rows - mineCount;
-var isGameOver = false;
+var flagCountValue = mineCount; //number of flags the user had remaining
+var tiles = []; //array of just tiles, no mines
+var coordinates = null; //2D array containing all the tiles
+var mines = []; //array of mines
+var isFirstTurn = true; //true when it is the users first turn
+var remainingTiles = columns * rows - mineCount; //number of remaining tiles that are not mines
+var isGameOver = false; //when true the user will be unable to select any buttons
 
 function buildGrid() {
     // Fetch grid and clear out old elements.
@@ -74,20 +69,16 @@ function generateMines() {
         tiles.splice( idx, 1 );
 
         mines.push(coordinate);
-        console.log( coordinate.y + ":" + coordinate.x );
     }   
 }
 
+//start a new game
 function startGame() {
-    //reset variables in case new game starts
     resetGame();
-
-    // update remaining bombs
-    document.getElementById("flagCount").innerHTML = flagCountValue;
-
     buildGrid();
     generateMines();
     smileyAlive();
+    document.getElementById("flagCount").innerHTML = flagCountValue;
 }
 
 function handleTileClick(event) {
@@ -144,27 +135,26 @@ function smileyWin() {
     smiley.classList.add("face_win");
 }
 
+function updateNotification(message) {
+    document.getElementById("notification").innerHTML = message;
+}
+
+//change the game parameters base on the users difficulty input
 function setDifficulty() {
     var difficultySelector = document.getElementById("difficulty");
     var difficulty = difficultySelector.selectedIndex;
 
-    if(difficulty == 0)
-    {
-        console.log("easy");
+    if(difficulty == 0) {
         mineCount = 10;
         rows = 9;
         columns = 9;
     }
-    else if(difficulty == 1)
-    {
-        console.log("medium");
+    else if(difficulty == 1) {
         mineCount = 40;
         rows = 16;
         columns = 16;
     }
-    else if(difficulty == 2)
-    {
-        console.log("hard");
+    else if(difficulty == 2) {
         mineCount = 99;
         rows = 30;
         columns = 16;
@@ -189,6 +179,46 @@ function updateTimer() {
     document.getElementById("timer").innerHTML = timeValue;
 }
 
+//checks the type of tile that the user has selected
+function checkTile(elem) {
+     if (!elem.classList.contains("flag")) {
+        if(coordinates[elem.x][elem.y].mine && !isFirstTurn)
+            mineFound(elem)
+        else
+            displayTile(elem)
+     }    
+}
+
+//add and remove flag on the tile that the user has selected
+function updateFlag(elem) {
+    if(!coordinates[elem.x][elem.y].revealed) {
+        if(elem.classList.contains("flag")) {
+        elem.classList.remove("flag");
+        elem.classList.add("hidden");
+        flagCountValue++;
+        } else {
+            elem.classList.remove("hidden");
+            elem.classList.add("flag");
+            flagCountValue--;
+        }
+        document.getElementById("flagCount").innerHTML = flagCountValue;
+    }
+}
+
+//reveals the tiles that are adjancet to the selected tile
+function revealAdjacentTiles(elem) {
+    var x = elem.x == 0 ? elem.x : elem.x - 1;
+    for( ; x <= elem.x + 1 && x < coordinates.length; x++) {
+        var y = elem.y == 0 ? elem.y : elem.y-1;
+        for( ; y <= elem.y + 1 && y < coordinates[x].length; y++) {
+            if(!coordinates[x][y].revealed) {
+                checkTile(coordinates[x][y]);
+            }
+        }
+    }
+}
+
+//calls functions to display all the mines when a mine is selected
 function mineFound(elem) {
     displayAllMines();
     displayWrongFlags();
@@ -197,6 +227,30 @@ function mineFound(elem) {
     gameOver();
 }
 
+
+//iterate through the mines array to reveal all the mines
+function displayAllMines() {
+    for (var i = 0; i < mines.length; i++) {
+        if(!mines[i].classList.contains("flag")) {
+            mines[i].classList.remove("hidden");
+            mines[i].classList.add("mine");
+        }
+    }
+}
+
+//iterate through the coordinates to see if there is a flag on a tile that doesnt have a mine
+function displayWrongFlags() {
+    for(var x = 0; x < coordinates.length; x++) {
+        for(var y = 0 ; y < coordinates[x].length; y++) {
+            if(!coordinates[x][y].mine && coordinates[x][y].classList.contains("flag")) {
+                coordinates[x][y].classList.remove("flag");
+                coordinates[x][y].classList.add("mine_marked");
+            }
+        }
+    }
+}
+
+//Sets the tiles number based on the number of neighbouring mines
 function displayTile(elem) {
     var neighbourMines = getNumNeighbourMines( elem.x, elem.y );
    
@@ -241,63 +295,12 @@ function displayTile(elem) {
         expand( elem.x, elem.y );
     }
 
+    //if there are no more tiles to be revealed then the user has won
     if(remainingTiles == 0)
         winner();
 }
 
-function gameOver() {
-    isGameOver = true;
-    stopTimer();
-    updateNotification("You Lose! Try again");
-    smileyDead();
-}
-
-function displayAllMines() {
-    for (var i = 0; i < mines.length; i++) {
-        if(!mines[i].classList.contains("flag")) {
-            mines[i].classList.remove("hidden");
-            mines[i].classList.add("mine");
-        }
-    }
-}
-
-function displayWrongFlags() {
-    for(var x = 0; x < coordinates.length; x++) {
-        for(var y = 0 ; y < coordinates[x].length; y++) {
-            if(!coordinates[x][y].mine && coordinates[x][y].classList.contains("flag")) {
-                coordinates[x][y].classList.remove("flag");
-                coordinates[x][y].classList.add("mine_marked");
-            }
-        }
-    }
-}
-
-function updateFlag(elem) {
-    if(elem.classList.contains("flag")) {
-        elem.classList.remove("flag");
-        elem.classList.add("hidden");
-        flagCountValue++;
-    } else {
-        elem.classList.remove("hidden");
-        elem.classList.add("flag");
-        flagCountValue--;
-    }
-    document.getElementById("flagCount").innerHTML = flagCountValue;
-}
-
-function revealAdjacentTiles(elem) {
-    elem.classList.remove("hidden");
-}
-
-function checkTile(elem) {
-     if (!elem.classList.contains("flag")) {
-        if(coordinates[elem.x][elem.y].mine && !isFirstTurn)
-            mineFound(elem)
-        else
-            displayTile(elem)
-     }    
-}
-
+//iterate over the adjacent tiles to see how many of them are mines
 function getNumNeighbourMines(elemX, elemY) {
     var mineCount = 0;
         
@@ -313,6 +316,19 @@ function getNumNeighbourMines(elemX, elemY) {
     return mineCount;
 }
 
+function expand( elemX, elemY ) {
+    var x = elemX == 0 ? elemX : elemX - 1;
+    for( ; x <= elemX + 1 && x < coordinates.length; x++) {
+        var y = elemY == 0 ? elemY : elemY-1;
+        for( ; y <= elemY + 1 && y < coordinates[x].length; y++) {
+            if(!coordinates[x][y].mine && !coordinates[x][y].revealed && !coordinates[x][y].classList.contains("flag")) {
+                displayTile(coordinates[x][y]);
+            }
+        }
+    }
+}
+
+//resets all the variables in order to start a new game
 function resetGame() {
     tiles = [];
     coordinates = null;
@@ -326,18 +342,6 @@ function resetGame() {
     updateTimer();
 }
 
-function expand( elemX, elemY ) {
-    var x = elemX == 0 ? elemX : elemX - 1;
-    for( ; x <= elemX + 1 && x < coordinates.length; x++) {
-        var y = elemY == 0 ? elemY : elemY-1;
-        for( ; y <= elemY + 1 && y < coordinates[x].length; y++) {
-            if(!coordinates[x][y].mine && !coordinates[x][y].revealed && !coordinates[x][y].classList.contains("flag")) {
-                displayTile(coordinates[x][y]);
-            }
-        }
-    }
-}
-
 function winner() {
     isGameOver = true;
     updateNotification(`Congratulations, You Win! Your score was ${timeValue}.`);
@@ -345,6 +349,9 @@ function winner() {
     smileyWin();
 }
 
-function updateNotification(message) {
-    document.getElementById("notification").innerHTML = message;
+function gameOver() {
+    isGameOver = true;
+    stopTimer();
+    updateNotification("You Lose! Try again");
+    smileyDead();
 }
